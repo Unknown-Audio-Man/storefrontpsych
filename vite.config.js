cd340a5e4c331942ac24146a36d35d7a0a2d4831
Unknown-Audio-Man <roller_pretty.0p@icloud.com>
2026-04-25 14:55:17 +0530

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/storefrontpsych/',
+  // Add this line matching your GitHub repository name exactly:
+  base: '/storefrontpsych/', 
 })
